6591d9a848681dd0c5227f62483635c162373e28
JoaoFarisseChirindja <75274423+JoaoFarisseChirindja@users.noreply.github.com>
2021-03-28 13:59:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Learning_Json-2
---
+++ index.js
@@ -0,0 +1,28 @@
+import JSON
+
+{
+    "fullname": "Joao Farisse",
+    "nickname": "Djuas",
+    "surname": "Chirindja",
+    "name": "Joao",
+    "date": "2021-03-22T00:00:00",
+    "membership ": [
+        {
+            "relationship": "father",
+            "name": "Farisse Joao Chirindja"
+        },
+        {
+            "relationship": "mother",
+            "name": "Laura Chirindja"
+        }
+    ],
+     {"placeofbirth": "Moçambicano",
+    "address": "Matola D",
+    "contact": "+258846295393",
+    "profession": "IT",
+    "workplace": "Mahota Services",
+    "identifier": "BI nº 45723453323E",
+    "drivinglicense": {
+        "type": "Pesados"
+    }
+}
